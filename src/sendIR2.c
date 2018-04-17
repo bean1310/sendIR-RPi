937e6bc279d49afa741fb9a044d847f0737a60f5
bean1310 <22887835+bean1310@users.noreply.github.com>
2018-04-17 23:23:23 +0900

    [fix] 送信時のプログラムの最適化

--- src/sendIR2.c
+++ src/sendIR2.c
@@ -19,23 +19,42 @@
 // GPIO指定をわかりやすくする自作ヘッダファイル
 #include "rpi-pin.h"
 
+/* OnTime, OffTimeを保存する構造体 */
+struct onofftime{
+
+	unsigned int onTime;
+	unsigned int offTime;
+	struct onofftime *next;
+
+};
+
 // 赤外線センサのOutputピンに接続するGPIOピンの指定
 const unsigned int SEND_PIN = GPIO_18;
 
 // 赤外線信号を繰り返し送信する回数.
-const unsigned int REPEAT_SEND_CODE = 3;
+const unsigned int REPEAT_SEND_CODE = 1;
 
 // 変調単位の設定(マイクロ秒)
 const unsigned int MODULATION_TIME = 562;
 
 const unsigned int DUTY = 3;
 
+void freeOnOffTime(struct onofftime *p) {
+	
+	if(p -> next != NULL) freeOnOffTime(p -> next);
+
+	free(p);
+
+}
+
 int main(int argc, char *argv[]){
 
     FILE *fp;
+	struct onofftime onOffTime = {0, 0, NULL};
+	struct onofftime *tmpAddr = NULL;
     char fileName[128];
     int onTime, offTime, signalBit;
-    int i, n;
+    int n;
 
     if (argc > 1 && argc < 3) {
         strcpy(fileName, argv[1]);
@@ -50,42 +69,64 @@ int main(int argc, char *argv[]){
 
     if(wiringPiSetup() == -1) return 1;
 
+	/* SEND_PINの初期設定 */
     pinMode(SEND_PIN, PWM_OUTPUT);
     pwmSetMode(PWM_MODE_MS);
     pwmSetRange(DUTY);
     pwmSetClock((int)(19200 / 38 / DUTY));
 
-    for(n = 0; n < REPEAT_SEND_CODE; n++) {
+	/* リーダコード部分は読み込まない */
+	fseek(fp, (int)sizeof(int) * 7, SEEK_SET);
+	
+	tmpAddr = &onOffTime;
 
-        /* Send Leader Code*/
+    while(fscanf(fp, "%d %d", &onTime, &offTime) != EOF) {
+	
+		struct onofftime *newAddr = (struct onofftime *)malloc(sizeof(struct onofftime));
+		newAddr -> onTime = onTime;
+		newAddr -> offTime = offTime;
+		newAddr -> next = NULL;
+
+		tmpAddr -> next = newAddr;
+		tmpAddr = newAddr;
+
+	}
+
+    for(n = 0; n < REPEAT_SEND_CODE; n++) {		
+
+		tmpAddr = onOffTime.next;
+
+        /* Send Leader Code */
         pwmWrite(SEND_PIN, 1);
         delayMicroseconds(MODULATION_TIME * 16);
         pwmWrite(SEND_PIN, 0);
         delayMicroseconds(MODULATION_TIME * 8);
+		
+		while( tmpAddr != NULL ){
+		
+			// データの整形
+        	signalBit = tmpAddr -> offTime / tmpAddr -> onTime >= 2 ? 1 : 0;
 
-        // Send customer and data code.
-        rewind(fp);
-
-        fseek(fp, (int)sizeof(int) * 7, SEEK_SET);
-
-        while(fscanf(fp, "%d %d", &onTime, &offTime) != EOF) {
-
-            printf("%d %d\n", onTime, offTime);
-
-            // データの整形
-            signalBit = offTime / onTime >= 2 ? 1 : 0;
-
-            // データの送信
-            pwmWrite(SEND_PIN, 1);
-            delayMicroseconds(MODULATION_TIME);
-            pwmWrite(SEND_PIN, 0);
-            delayMicroseconds(MODULATION_TIME * ( 1 + signalBit * 2 ) );
-
-        }
+        	// データの送信
+        	pwmWrite(SEND_PIN, 1);
+        	delayMicroseconds(MODULATION_TIME);
+        	pwmWrite(SEND_PIN, 0);
+        	delayMicroseconds(MODULATION_TIME * ( 1 + signalBit * 2 ) );
+	
+#ifdef DEBUG
+			printf("onTime: %d, offTime: %d, send code is %d\n", tmpAddr -> onTime, tmpAddr -> offTime, signalBit);
+#endif
+			tmpAddr = tmpAddr -> next;
+		
+		}
 
     }
 
     fclose(fp);
+	
+	tmpAddr = onOffTime.next;
+
+	freeOnOffTime(tmpAddr);
 
     return 0;
 }
